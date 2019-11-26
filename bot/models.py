f24f91b7ae242adc30b53e996325fc71307957dc
byYuraGudan <yura.gudan@gmail.com>
2019-11-27 01:31:19 +0200

    create models

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+
+class TelegramUser(models.Manager):
+    full_name = models.CharField(max_length=100)
+    datetime = models.DateTimeField(auto_now=True)
+    state = models.CharField(max_length=100)
+    blocked = models.BooleanField(default=False)
+
+
+class TypeBook(models.Manager):
+    name = models.CharField(max_length=100, null=True)
+    hidden = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name)
+
+
+class File(models.Manager):
+    name = models.CharField(max_length=100, null=False)
+    file = models.BinaryField(null=False)
+    type = models.BooleanField()
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name)
+
+
+class Author(models.Manager):
+    full_name = models.CharField(max_length=100, null=True)
+    hidden = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.full_name)
+
+
+class Book(models.Manager):
+    type_id = models.ForeignKey(TypeBook, on_delete=models.PROTECT)
+    file_id = models.ForeignKey(File, on_delete=models.PROTECT)
+    author_id = models.ForeignKey(Author, on_delete=models.PROTECT)
+    name = models.CharField(null=False)
+    year = models.DateField()
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name)
+
+
+class Basket(models.Manager):
+    datetime = models.DateTimeField(auto_now=True)
+    telegram_user_id = models.ForeignKey(TelegramUser, on_delete=models.PROTECT)
+    tolal_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.telegram_user_id)
+
+
+class ListBasket(models.Manager):
+    basket_id = models.ForeignKey(Basket, on_delete=models.PROTECT)
+    book_id = models.ForeignKey(Book, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return '{}.{} - {}'.format(self.basket_id, self.id, self.book_id)
+
+
+class Like(models.Manager):
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    telegram_user_id = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now=True)
+    rating = models.PositiveSmallIntegerField(default=10)
+
+    def __str__(self):
+        return '{}.{} - {}'.format(self.telegram_user_id, self.book_id, self.rating)
