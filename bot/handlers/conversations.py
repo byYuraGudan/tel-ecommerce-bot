6496c033b110891a6df13e46f7fb5cf31dd33521
byYuraGudan <yura.gudan@gmail.com>
2019-11-28 01:51:22 +0200

    added info catalog and info book

--- bot/handlers/conversations.py
+++ bot/handlers/conversations.py
@@ -1,12 +1,19 @@
+import json
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import *
+
+from bot import models as bot_models
+from bot.keyboards import keyboards
 
 
 class BaseConversationHandler(ConversationHandler):
     STATE = None
 
     def __init__(self, *args, **kwargs):
-        super(BaseConversationHandler, self).__init__(self.entry_points(), self.states(), self.fallbacks(), *args,
-                                                      persistent=True, name=self.__class__.__name__, **kwargs)
+        super(BaseConversationHandler, self).__init__(
+            self.entry_points(), self.states(), self.fallbacks(), *args, **kwargs
+        )
 
     @property
     def states(self):
@@ -19,3 +26,60 @@ class BaseConversationHandler(ConversationHandler):
     @property
     def fallbacks(self):
         raise NotImplementedError
+
+    def exit(self, bot, update):
+        update.effective_message.reply_text('Виберіть що вас цікавить.', reply_markup=keyboards.main_keyboard())
+        return ConversationHandler.END
+
+
+class TypeBookConversation(BaseConversationHandler):
+    STATE = ['catalog', 'catalog-books', 'book-info']
+
+    def entry_points(self):
+        return [MessageHandler(Filters.regex('^Каталог+'), self.catalog)]
+
+    def states(self):
+        states = {
+            self.STATE[0]: [CallbackQueryHandler(self.catalog_books)],
+            self.STATE[1]: [CallbackQueryHandler(self.book_info)]
+        }
+        return states
+
+    def fallbacks(self):
+        return [MessageHandler(Filters.regex("^Назад+"), self.exit)]
+
+    def catalog(self, bot, update):
+        catalogs = bot_models.TypeBook.objects.exclude(hidden=True).values('id', 'name')
+        if not catalogs:
+            update.effective_message.reply_text('Нажаль немає каталогів', reply_markup=keyboards.main_keyboard())
+
+        keyboards_markup = [
+            InlineKeyboardButton(catalog['name'], callback_data=str(catalog['id'])) for catalog in catalogs
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboards.build_menu(keyboards_markup))
+        update.effective_message.reply_text('Виберіть цікавий вам каталог.', reply_markup=reply_markup)
+        return self.STATE[0]
+
+    def catalog_books(self, bot, update):
+        query = update.callback_query
+        data = json.loads(query.data)
+        books = bot_models.Book.objects.filter(type_id__id=data).values('id', 'name')
+
+        keyboards_markup = [
+            InlineKeyboardButton(book['name'], callback_data=str(book['id'])) for book in books
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboards.build_menu(keyboards_markup))
+        query.edit_message_text(text="Виберіть цікаву вам книжки.", reply_markup=reply_markup)
+        return self.STATE[1]
+
+    def book_info(self, bot, update):
+        query = update.callback_query
+        data = json.loads(query.data)
+        book = bot_models.Book.objects.filter(id=data).values()
+        keyboards_markup = [
+            InlineKeyboardButton('Преглянуту інформацію', callback_data='show_data'),
+            InlineKeyboardButton('Додати до корзини', callback_data='add_basket'),
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboards.build_menu(keyboards_markup))
+        query.edit_message_text(text="Інформація про книжку: \n {}".format(dict(book)), reply_markup=reply_markup)
+        return ConversationHandler.END
--- bot/keyboards/keyboards.py
+++ bot/keyboards/keyboards.py
@@ -2,9 +2,18 @@ from telegram import *
 
 
 def main_keyboard():
-    keyboards = [
-        ['Новинки 🆘🆕', 'Топ книжок ‼️🔥'],
-        ['Жанри книг 📚'],
-        ['Корзина 🛒']
-    ]
-    return ReplyKeyboardMarkup(keyboards, resize_keyboard=True)
+    keyboards = ['Новинки 🆘🆕', 'Топ книжок ‼️🔥', 'Каталог 📚', 'Корзина 🛒']
+    return ReplyKeyboardMarkup(build_menu(keyboards), resize_keyboard=True)
+
+
+def build_menu(buttons, n_cols=2, header_buttons=None, footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, [header_buttons])
+    if footer_buttons:
+        menu.append([footer_buttons])
+    return menu
+
+
+def build_keyboard_markup(buttons, **kwargs):
+    return ReplyKeyboardMarkup(build_menu(buttons, **kwargs), resize_keyboard=True)
